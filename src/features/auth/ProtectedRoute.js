ce631357996d585a508bffb2614542e03c0508d5
bhakti.palkar <bhakti.palkar@macs-g.com>
2024-03-08 16:36:52 +0530

    first commit

---
+++ src/features/auth/ProtectedRoute.js
@@ -0,0 +1,18 @@
+import { Navigate, Outlet, useLocation } from "react-router-dom"
+import useAuth from "../../hooks/useAuth"
+
+
+
+const ProtectedRoute = ({allowedRoles}) => {
+    const location = useLocation()
+    const {roles} = useAuth()
+
+    const content = (
+        roles.some(role => allowedRoles.includes(role)) ? <Outlet /> : <Navigate to='/login' state= {{ from: location}}  replace />
+    )
+
+
+  return content
+}
+
+export default ProtectedRoute
